c11eeede6fb90d09dd876e6025ed2c04d49701c2
Zakaria <zakaria.bentanfous@gmail.com>
2026-04-19 11:34:50 +0200

    refactor: hexadecimals utils

--- tests/lorachat/main.c
+++ tests/lorachat/main.c
@@ -42,6 +42,8 @@
 
 #include "fmt.h"
 
+#include "hex.h"
+
 #define SX127X_LORA_MSG_QUEUE (16U)
 #ifndef SX127X_STACKSIZE
 #define SX127X_STACKSIZE (THREAD_STACKSIZE_DEFAULT)
@@ -58,42 +60,6 @@ static bool rxhex_enabled;
 static sx127x_t sx127x;
 
 static bool echo_enabled = false;
-
-static size_t convert_hex(uint8_t *dest, size_t max_len, const char *src)
-{
-    size_t src_len = strlen(src);
-    size_t out_len = 0;
-
-    if ((src_len == 0) || ((src_len % 2) != 0))
-    {
-        return 0;
-    }
-
-    for (size_t i = 0; (i < src_len) && (out_len < max_len); i += 2)
-    {
-        int value;
-        if (sscanf(src + i, "%2x", &value) != 1)
-        {
-            return 0;
-        }
-        dest[out_len++] = (uint8_t)value;
-    }
-
-    if (out_len != (src_len / 2))
-    {
-        return 0;
-    }
-
-    return out_len;
-}
-
-static void print_hex_payload(const uint8_t *buf, size_t len)
-{
-    for (size_t i = 0; i < len; i++)
-    {
-        printf("%02X", buf[i]);
-    }
-}
 
 int lora_setup_cmd(int argc, char **argv)
 {
